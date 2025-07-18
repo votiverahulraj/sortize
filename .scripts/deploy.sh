47b47b457742b222d4b67265a03632079cd39e00
root <root@gifted-yalow.216-10-243-87.plesk.page>
2025-07-18 03:10:45 -0400

    update changes test

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 echo "Deployment started ..."
 
 # Pull the latest version of the app
-git pull origin dev
+git pull origin main
 echo "New changes copied to server !"
 
 # Reloading Application So New Changes could reflect on website
