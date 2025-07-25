fc91bcf8ce2bc9cba8fc446bfef46d1786fce441
Kokil'soni <votivephp.kokil@gmail.com>
2025-07-25 18:59:44 +0530

    add defaul profile image

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <!-- ✅ Meta -->
     <meta charset="utf-8">
@@ -21,7 +22,8 @@
 
     <!-- ✅ Select2 -->
     <link rel="stylesheet" href="{{ url('/public') }}/admin_assets/vendors/select2/select2.min.css">
-    <link rel="stylesheet" href="{{ url('/public') }}/admin_assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+    <link rel="stylesheet"
+        href="{{ url('/public') }}/admin_assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
 
     <!-- ✅ Bootstrap + DataTables -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
@@ -54,96 +56,107 @@
 
     <div class="container-scroller">
 
-      <!-- partial:partials/_navbar.html -->
-      <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
-        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-            <a class="navbar-brand brand-logo me-5" href="{{route('admin.dashboard')}}"><img src="{{ url('/public') }}/admin_assets/images/Sortiz.png" class="me-2" alt="logo" /></a>
-            <a class="navbar-brand brand-logo-mini" href="{{route('admin.dashboard')}}"><img src="{{ url('/public') }}/admin_assets/images/fav.png" alt="logo" /></a>
+        <!-- partial:partials/_navbar.html -->
+        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+                <a class="navbar-brand brand-logo me-5" href="{{ route('admin.dashboard') }}"><img
+                        src="{{ url('/public') }}/admin_assets/images/Sortiz.png" class="me-2" alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="{{ route('admin.dashboard') }}"><img
+                        src="{{ url('/public') }}/admin_assets/images/fav.png" alt="logo" /></a>
 
-        </div>
-        <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-            <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-            <span class="icon-menu"></span>
-            </button>
+            </div>
+            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                    <span class="icon-menu"></span>
+                </button>
 
-            <ul class="navbar-nav navbar-nav-right">
-            <li class="nav-item dropdown">
-                <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
-                <i class="icon-bell mx-0"></i>
-                <span class="count"></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-                <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
-                <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                    <div class="preview-icon bg-success">
-                        <i class="ti-info-alt mx-0"></i>
-                    </div>
-                    </div>
-                    <div class="preview-item-content">
-                    <h6 class="preview-subject font-weight-normal">Application Error</h6>
-                    <p class="font-weight-light small-text mb-0 text-muted"> Just now </p>
-                    </div>
-                </a>
-                <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                    <div class="preview-icon bg-warning">
-                        <i class="ti-settings mx-0"></i>
-                    </div>
-                    </div>
-                    <div class="preview-item-content">
-                    <h6 class="preview-subject font-weight-normal">Settings</h6>
-                    <p class="font-weight-light small-text mb-0 text-muted"> Private message </p>
-                    </div>
-                </a>
-                <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                    <div class="preview-icon bg-info">
-                        <i class="ti-user mx-0"></i>
-                    </div>
-                    </div>
-                    <div class="preview-item-content">
-                    <h6 class="preview-subject font-weight-normal">New user registration</h6>
-                    <p class="font-weight-light small-text mb-0 text-muted"> 2 days ago </p>
-                    </div>
-                </a>
-                </div>
-            </li>
-            <li class="nav-item nav-profile dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                <img src="{{ url('/public') }}/admin_assets/images/faces/face28.jpg" alt="profile" />
-                </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-                <a class="dropdown-item">
-                    <i class="ti-settings text-primary"></i> Settings </a>
-                <a class="dropdown-item" href="{{route('admin.logout')}}">
-                    <i class="ti-power-off text-primary"></i> Logout </a>
-                </div>
-            </li>
+                <ul class="navbar-nav navbar-nav-right">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#"
+                            data-bs-toggle="dropdown">
+                            <i class="icon-bell mx-0"></i>
+                            <span class="count"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
+                            aria-labelledby="notificationDropdown">
+                            <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-success">
+                                        <i class="ti-info-alt mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">Application Error</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted"> Just now </p>
+                                </div>
+                            </a>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-warning">
+                                        <i class="ti-settings mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">Settings</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted"> Private message </p>
+                                </div>
+                            </a>
+                            <a class="dropdown-item preview-item">
+                                <div class="preview-thumbnail">
+                                    <div class="preview-icon bg-info">
+                                        <i class="ti-user mx-0"></i>
+                                    </div>
+                                </div>
+                                <div class="preview-item-content">
+                                    <h6 class="preview-subject font-weight-normal">New user registration</h6>
+                                    <p class="font-weight-light small-text mb-0 text-muted"> 2 days ago </p>
+                                </div>
+                            </a>
+                        </div>
+                    </li>
+                    <li class="nav-item nav-profile dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+                            id="profileDropdown">
+                            @php
+                                $profileImg = auth()->user()->profile_image;
+                                $avataar = $profileImg ? $profileImg : 'default_profile.png';
+                            @endphp
+                            <img src="{{ url('/public/uploads/profile_image/' . $avataar) }}" alt="profile" />
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
+                            aria-labelledby="profileDropdown">
+                            <a class="dropdown-item">
+                                <i class="ti-settings text-primary"></i> Settings </a>
+                            <a class="dropdown-item" href="{{ route('admin.logout') }}">
+                                <i class="ti-power-off text-primary"></i> Logout </a>
+                        </div>
+                    </li>
 
-            </ul>
+                </ul>
 
-            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center add-open-close" type="button">
-            <span class="icon-menu"></span>
-            </button>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center add-open-close"
+                    type="button">
+                    <span class="icon-menu"></span>
+                </button>
 
-        </div>
-      </nav>
-      <!-- partial -->
-      <div class="container-fluid page-body-wrapper">
-        <!-- partial:partials/_sidebar.html -->
-        @include('admin.layouts.sidebar')
+            </div>
+        </nav>
         <!-- partial -->
-        @yield('content')
-        <!-- main-panel ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_sidebar.html -->
+            @include('admin.layouts.sidebar')
+            <!-- partial -->
+            @yield('content')
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
     </div>
 
 
     <!-- ✅ Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-     {{-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> --}}
+    {{-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> --}}
     <script src="{{ url('/public') }}/admin_assets/vendors/js/vendor.bundle.base.js"></script>
     <script src="{{ url('/public') }}/admin_assets/vendors/select2/select2.min.js"></script>
     <script src="{{ url('/public') }}/admin_assets/js/select2.js"></script>
@@ -175,15 +188,15 @@
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script> --}}
 
 
-{{--
+    {{--
  <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script> --}}
- <script src="https://cdn.datatables.net/buttons/3.2.4/js/dataTables.buttons.js"></script>
- <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.dataTables.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
- <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.html5.min.js"></script>
- <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.4/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.2.4/js/buttons.print.min.js"></script>
 
 
     <!-- ✅ Select extension -->
@@ -208,4 +221,5 @@
 
     @stack('scripts')
 </body>
+
 </html>
