3652288d1a148e1ecd5bd1cfcc4b181c0789108b
Kokil'soni <votivephp.kokil@gmail.com>
2025-07-25 14:08:27 +0530

    migration and seeder webhook

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        
+        $this->call([
+            EventCategorySeeder::class
+        ]);
     }
 }
